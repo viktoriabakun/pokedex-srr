e63b9744d3e27a1d8671ce4252f3e37c3fd7a96b
Viktoryia <viktoriabakun00@gmail.com>
2022-09-16 17:44:11 +0400

    feat: add spherical pokemon card

--- src/common/components/pokemon/card/index.tsx
+++ src/common/components/pokemon/card/index.tsx
@@ -1,6 +1,6 @@
 import capitalise from 'lodash';
 import type { FC } from 'react';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { IPokemon } from '@interfaces/pokemon';
 import styles from './styles.module.scss';
 
@@ -12,21 +12,26 @@ const PokemonCard: FC<IPokemonCard> = ({ cardData }) => {
   const { id, name, sprites, types } = cardData;
 
   const nameCapitalized = capitalise(name);
-
   const typeList = types.map((t) => t.type.name).join(', ');
+  const pokemonImg = sprites?.other['official-artwork'].front_default;
+  const pokemonGif = sprites?.versions['generation-v']['black-white']['animated']['front_default'];
+
+  const [avatar, setAvatar] = useState(pokemonImg);
+
+  const setGifAvatar = useCallback(() => setAvatar(pokemonGif), [pokemonGif]);
+  const setImgAvatar = useCallback(() => setAvatar(pokemonGif), [pokemonGif]);
 
   return (
-    <div className={styles.container}>
-      <header>
-        <img className={styles.avatar} src={sprites.front_default} alt="pokemon" />
-      </header>
+    <div className={styles.itemContainer} onMouseEnter={setGifAvatar} onMouseLeave={setImgAvatar}>
+      <h2 className={`${styles.ballHalf} ${styles.top}`}>
+        #{id} <strong>{nameCapitalized}</strong>
+      </h2>
 
-      <main className={styles.details}>
-        <h1>
-          #{id} <strong>{nameCapitalized}</strong>
-        </h1>
-        <section>Types: {typeList}</section>
-      </main>
+      <div className={styles.avatarContainer}>
+        <img src={avatar} className={styles.avatar} alt="pokemon" />
+      </div>
+
+      <p className={`${styles.ballHalf} ${styles.bottom}`}>{typeList}</p>
     </div>
   );
 };
--- src/interfaces/pokemon.ts
+++ src/interfaces/pokemon.ts
@@ -1,11 +1,11 @@
 /**
- * Pokemon entities
+ * Pokemon
  */
 export interface IPokemon {
   id: number;
   name: string;
   types: IPokemonType[];
-  sprites: Record<string, string>;
+  sprites: IPokemonSprite;
 }
 
 /**
@@ -16,5 +16,32 @@ export interface IPokemonType {
   type: {
     name: string;
     url: string;
+  };
+}
+
+/**
+ * Images or gifs of pokemon
+ */
+interface IPokemonSprite {
+  ['front_default']: string;
+  other: {
+    ['official-artwork']: {
+      ['front_default']: string;
+    };
+  };
+  versions: {
+    ['generation-v']: {
+      ['black-white']: {
+        animated: {
+          ['back_default']: string;
+          ['back_shiny']: string;
+          ['back_shiny_female']: string;
+          ['front_default']: string;
+          ['front_female']: string;
+          ['front_shiny']: string;
+          ['front_shiny_female']: string;
+        };
+      };
+    };
   };
 }
